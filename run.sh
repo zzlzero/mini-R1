1f8df069cdfd98100f637432a126a44107c51405
yz-qiang <1214057175@qq.com>
2025-02-18 03:14:40 +0000

    更新运行脚本以增加进程数至7并修正配置文件参数

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-accelerate launch --num_processes 3 --./deepspeed_zero3.yaml ./run_r1_grpo.py --config ./grpo-qwen-2.5-3b-deepseek-r1-countdown.yaml
+accelerate launch --num_processes 7 --config_file ./deepspeed_zero3.yaml ./run_r1_grpo.py --config ./grpo-qwen-2.5-3b-deepseek-r1-countdown.yaml
